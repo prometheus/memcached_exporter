14b2b4d77ba8eed82e6fe49325e051730f5bfd8e
Fumihiro Itoh <fmhrit@gmail.com>
2019-04-17 19:58:39 +0900

    returns memcached_up = 0 when fail to parse a stats values.
    Signed-off-by: Fumihiro Itoh <fmhrit@gmail.com>

--- main_test.go
+++ main_test.go
@@ -17,6 +17,7 @@ import (
 	"bytes"
 	"context"
 	"io/ioutil"
+	"net"
 	"net/http"
 	"os"
 	"os/exec"
@@ -25,6 +26,7 @@ import (
 	"time"
 
 	"github.com/grobie/gomemcache/memcache"
+	"github.com/prometheus/client_golang/prometheus"
 )
 
 func TestAcceptance(t *testing.T) {
@@ -144,4 +146,56 @@ OUTER:
 	cancel()
 
 	<-errc
+}
+
+func TestParseStatsSettings(t *testing.T) {
+	addr, err := net.ResolveIPAddr("ip4", "127.0.0.1")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		t.Parallel()
+
+		var statsSettings = map[net.Addr]map[string]string{
+			addr: {
+				"maxconns":              "10",
+				"lru_crawler":           "yes",
+				"lru_crawler_sleep":     "100",
+				"lru_crawler_tocrawl":   "0",
+				"lru_maintainer_thread": "no",
+				"hot_lru_pct":           "20",
+				"warm_lru_pct":          "40",
+				"hot_max_factor":        "0.20",
+				"warm_max_factor":       "2.00",
+			},
+		}
+		ch := make(chan prometheus.Metric, 100)
+		e := NewExporter("", 100*time.Millisecond)
+		if err := e.parseStatsSettings(ch, statsSettings); err != nil {
+			t.Errorf("expect return error, error: %v", err)
+		}
+	})
+
+	t.Run("Failure", func(t *testing.T) {
+		t.Parallel()
+		var statsSettings = map[net.Addr]map[string]string{
+			addr: {
+				"maxconns":              "10",
+				"lru_crawler":           "yes",
+				"lru_crawler_sleep":     "100",
+				"lru_crawler_tocrawl":   "0",
+				"lru_maintainer_thread": "fail",
+				"hot_lru_pct":           "20",
+				"warm_lru_pct":          "40",
+				"hot_max_factor":        "0.20",
+				"warm_max_factor":       "2.00",
+			},
+		}
+		ch := make(chan prometheus.Metric, 100)
+		e := NewExporter("", 100*time.Millisecond)
+		if err := e.parseStatsSettings(ch, statsSettings); err == nil {
+			t.Error("expect return error but not")
+		}
+	})
 }
